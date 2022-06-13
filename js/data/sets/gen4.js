36caf1cf122ba3d841ef3374621eb7f882d5a1b9
Shiva Devarajan <arcubedgs@gmail.com>
2022-06-13 17:49:23 -0400

    First Public Version

---
+++ js/data/sets/gen4.js
@@ -0,0 +1,18 @@
+/* AUTOMATICALLY GENERATED FROM @smogon/sets, DO NOT EDIT! */
+// var SETDEX_DPP = {"Abomasnow": {"Cynthia 1": {"item": "Expert Belt"}, "moves": {"Ice Shard", "Wood Hammer", "Protect", "Blizzard"}}}
+// var SETDEX_DPP = {"Abomasnow":{"OU Mixed":{"level":99,"ability":"Snow Warning","item":"Expert Belt","nature":"Lonely","evs":{"at":252,"sa":172,"sp":84},"moves":["Wood Hammer","Ice Shard","Hidden Power Fire","Protect"]}}}
+var SETDEX_DPP = {
+    "Togekiss":
+        { "Cynthia 1": { "level": 37, "item": "Lum Berry", "moves": ["Air Slash", "Aura Sphere", "Dazzling Gleam", "Nasty Plot"] }, "Cynthia 2": { "item": "Lum Berry", "moves": ["Moonblast", "Air Slash", "Heat Wave", "Follow Me"] } },
+    "Latias":
+        { "Cynthia 2": {"level": 100, "item": "Sitrus Berry", "moves": ["Psycho Boost", "Calm Mind", "Ice Beam", "Aura Sphere"]}},
+    "Arceus":
+        { "Cynthia 2": {"level": 100, "item": "Life Orb", "moves": ["Sacred Fire", "Giga Impact", "Extreme Speed", "Swords Dance"]}},
+    "Garchomp":
+        { "Cynthia 2": {"level": 100, "item": "Yache Berry", "moves": ["Dragon Rush", "Dragon Dance", "Earthquake", "Rock Slide"]}},
+    "Milotic":
+        { "Cynthia 2": {"level": 100, "item": "Leftovers", "moves": ["Water Spout", "Draco Meteor", "Ice Beam", "Recover"]}},
+    "Heatran":
+        { "Cynthia 2": {"level": 100, "item": "Focus Sash", "moves": ["Heat Wave", "Flash Cannon", "Earth Power", "Accelerock"]}},
+}
+// var SETDEX_DPP = {}
