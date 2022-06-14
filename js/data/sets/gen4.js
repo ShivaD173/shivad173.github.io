4420536211eb3eb686d319a6fc92e2f3493a82b3
Shiva Devarajan <arcubedgs@gmail.com>
2022-06-13 20:41:50 -0400

    Moves updated to 171 and E4 Movesets

--- js/data/sets/gen4.js
+++ js/data/sets/gen4.js
@@ -1,18 +1,66 @@
-/* AUTOMATICALLY GENERATED FROM @smogon/sets, DO NOT EDIT! */
-// var SETDEX_DPP = {"Abomasnow": {"Cynthia 1": {"item": "Expert Belt"}, "moves": {"Ice Shard", "Wood Hammer", "Protect", "Blizzard"}}}
-// var SETDEX_DPP = {"Abomasnow":{"OU Mixed":{"level":99,"ability":"Snow Warning","item":"Expert Belt","nature":"Lonely","evs":{"at":252,"sa":172,"sp":84},"moves":["Wood Hammer","Ice Shard","Hidden Power Fire","Protect"]}}}
+/* VGC Plat Trainer Sets */
 var SETDEX_DPP = {
     "Togekiss":
-        { "Cynthia 1": { "level": 37, "item": "Lum Berry", "moves": ["Air Slash", "Aura Sphere", "Dazzling Gleam", "Nasty Plot"] }, "Cynthia 2": { "item": "Lum Berry", "moves": ["Moonblast", "Air Slash", "Heat Wave", "Follow Me"] } },
+    {
+        "Cynthia 1": { "level": 37, "item": "Lum Berry", "moves": ["Air Slash", "Aura Sphere", "Dazzling Gleam", "Nasty Plot"] },
+        "Cynthia 2": { "item": "Lum Berry", "moves": ["Moonblast", "Air Slash", "Heat Wave", "Follow Me"] }
+    },
     "Latias":
-        { "Cynthia 2": {"level": 100, "item": "Sitrus Berry", "moves": ["Psycho Boost", "Calm Mind", "Ice Beam", "Aura Sphere"]}},
+        { "Cynthia 2": { "level": 100, "item": "Sitrus Berry", "moves": ["Psycho Boost", "Calm Mind", "Ice Beam", "Aura Sphere"] } },
     "Arceus":
-        { "Cynthia 2": {"level": 100, "item": "Life Orb", "moves": ["Sacred Fire", "Giga Impact", "Extreme Speed", "Swords Dance"]}},
+        { "Cynthia 2": { "level": 100, "item": "Life Orb", "moves": ["Sacred Fire", "Giga Impact", "Extreme Speed", "Swords Dance"] } },
     "Garchomp":
-        { "Cynthia 2": {"level": 100, "item": "Yache Berry", "moves": ["Dragon Rush", "Dragon Dance", "Earthquake", "Rock Slide"]}},
+        { "Cynthia 2": { "level": 100, "item": "Yache Berry", "moves": ["Dragon Rush", "Dragon Dance", "Earthquake", "Rock Slide"] } },
     "Milotic":
-        { "Cynthia 2": {"level": 100, "item": "Leftovers", "moves": ["Water Spout", "Draco Meteor", "Ice Beam", "Recover"]}},
+        { "Cynthia 2": { "level": 100, "item": "Leftovers", "moves": ["Water Spout", "Draco Meteor", "Ice Beam", "Recover"] } },
     "Heatran":
-        { "Cynthia 2": {"level": 100, "item": "Focus Sash", "moves": ["Heat Wave", "Flash Cannon", "Earth Power", "Accelerock"]}},
+        { "Cynthia 2": { "level": 100, "item": "Focus Sash", "moves": ["Heat Wave", "Flash Cannon", "Earth Power", "Accelerock"] } },
+    "Vaporeon":
+        { "Aaron": {"level": 100, "item": "Leftovers", "moves": ["Hydro Pump", "Ice Beam", "Follow Me", "Helping Hand"]}},
+    "Scizor":
+        { "Aaron": {"level": 100, "item": "Choice Band", "moves": ["Bullet Punch", "Bug Bite", "Superpower", "U-turn"]}},
+    "Ambipom":
+        { "Aaron": {"level": 100, "item": "Life Orb", "moves": ["Fake Out", "Last Resort"]}},
+    "Flygon":
+        { "Aaron": {"level": 100, "item": "Choice Scarf", "moves": ["Rock Slide", "U-turn", "Dragon Rush", "Earthquake"]}},
+    "Gliscor":
+        { "Aaron": {"level": 100, "item": "Toxic Orb", "moves": ["Earthquake", "Roost", "Brave Bird", "Swords Dance"]}},
+    "Heracross":
+        { "Aaron": {"level": 100, "item": "Flame Orb", "moves": ["Earthquake", "Close Combat", "Megahorn", "Protect"]}},
+    "Tyranitar":
+        { "Bertha": {"level": 100, "item": "Chople Berry", "moves": ["Earthquake", "Thunderbolt", "Rock Slide", "Protect"]}},
+    "Aerodactyl":
+        { "Bertha": {"level": 100, "item": "Sitrus Berry", "moves": ["Ice Fang", "Head Smash", "Tailwind", "Brave Bird"]}},
+    "Nidoking":
+        { "Bertha": {"level": 100, "item": "Flame Orb", "moves": ["Sucker Punch", "Head Smash", "Thunder Punch", "Bonemerang"]}},
+    "Mamoswine":
+        { "Bertha": {"level": 100, "item": "Lum Berry", "moves": ["Avalanche", "Rock Slide", "Iron Head", "Earthquake"]}},
+    "Rhyperior":
+        { "Bertha": {"level": 100, "item": "Focus Sash", "moves": ["Hammer Arm", "Rock Wrecker", "Endeavor", "Megahorn"]}},
+    "Aggron":
+        { "Bertha": {"level": 100, "item": "Focus Sash", "moves": ["Iron Tail", "Stone Edge", "Explosion", "Ice Punch"]}},
+    "Ninetales":
+        { "Flint": {"level": 100, "item": "Focus Sash", "moves": ["Flamethrower", "Solar Beam", "Shadow Ball", "Will-O-Wisp"]}},
+    "Charizard":
+        { "Flint": {"level": 100, "item": "Sitrus Berry", "moves": ["Heat Wave", "Solar Beam", "Air Slash", "Draco Meteor"]}},
+    "Moltres":
+        { "Flint": {"level": 100, "item": "Focus Sash", "moves": ["Heat Wave", "Ancient Power", "U-turn", "Ominous Wind"]}},
+    "Entei":
+        { "Flint": {"level": 100, "item": "Choice Scarf", "moves": ["Sacred Fire", "Stone Edge", "Extreme Speed", "Crunch"]}},
+    "Infernape":
+        { "Flint": {"level": 100, "item": "Life Orb", "moves": ["Blast Burn", "Gunk Shot", "Close Combat", "Mach Punch"]}},
+    "Leafeon":
+        { "Flint": {"level": 100, "item": "Miracle Seed", "moves": ["Leaf Blade", "Aerial Ace", "Swords Dance", "Synthesis"]}},
+    "Mewtwo":
+        { "Lucian": {"level": 100, "item": "Lum Berry", "moves": ["Psychic", "Icy Wind", "Aura Sphere", "Blizzard"]}},
+    "Cresselia":
+        { "Lucian": {"level": 100, "item": "Leftovers", "moves": ["Psychic", "Icy Wind", "Helping Hand", "Energy Ball"]}},
+    "Slowking":
+        { "Lucian": {"level": 100, "item": "Lum Berry", "moves": ["Focus Blast", "Fire Blast", "Yawn", "Muddy Water"]}},
+    "Alakazam":
+        { "Lucian": {"level": 100, "item": "Focus Sash", "moves": ["Focus Blast", "Psychic", "Energy Ball", "Recover"]}},
+    "Gallade":
+        { "Lucian": {"level": 100, "item": "Choice Scarf", "moves": ["Drain Punch", "Psycho Cut", "Leaf Blade", "Stone Edge"]}},
+    "Porygon-Z":
+        { "Lucian": {"level": 100, "item": "Choice Specs", "moves": ["Hyper Beam", "Psychic", "Ice Beam", "Focus Blast"]}},
 }
-// var SETDEX_DPP = {}
